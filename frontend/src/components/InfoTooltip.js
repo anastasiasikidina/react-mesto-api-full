02254c70f87ea99745c99832769df5079abb3b8c
Anastasia Sikidina <anastasia.sikidina@yandex.ru>
2021-12-13 14:56:35 +0300

    15 project

---
+++ frontend/src/components/InfoTooltip.js
@@ -0,0 +1,29 @@
+import failRegister from "../images/FailRegister.svg";
+import succesfulRegister from "../images/SuccesfulRegister.svg";
+ 
+export default function InfoToolTip({ onClose, isOpen, isRegSucces, name }) {
+  return (
+    <div className={isOpen ? `popup_opened popup` : `popup`}>
+      <button
+        onClick={onClose}
+        type="button"
+        className={`button-close popup__cross popup__cross_btn_close-${name}`}
+        aria-label="Закрыть попап"
+      ></button>
+      <div className="popup__content">
+      
+        <img
+          src={isRegSucces ? succesfulRegister : failRegister}
+          className="popup__info-icon"
+          alt="успех регистрации"
+        />
+        <h3 className="popup__title-info">
+          {isRegSucces
+            ? "Вы успешно зарегистрировались!"
+            : "Что-то пошло не так!\n Попробуйте еще раз."}
+        </h3>
+        
+      </div>
+    </div>
+  );
+}
